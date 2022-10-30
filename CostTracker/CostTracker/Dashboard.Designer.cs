5fb64a4491440171957ded0d3c73bc4c13534590
elzanataliw <elza.natali.w@mail.ugm.ac.id>
2022-10-31 00:05:58 +0700

    add ui (login and dashboard)

---
+++ CostTracker/CostTracker/Dashboard.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace CostTracker
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            this.pbHome = new System.Windows.Forms.PictureBox();
+            this.pbIncome = new System.Windows.Forms.PictureBox();
+            this.pbExpenses = new System.Windows.Forms.PictureBox();
+            this.pbLogout = new System.Windows.Forms.PictureBox();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.boxIncome = new System.Windows.Forms.PictureBox();
+            this.boxExpenses = new System.Windows.Forms.PictureBox();
+            this.boxBalance = new System.Windows.Forms.PictureBox();
+            this.btIncome = new System.Windows.Forms.PictureBox();
+            this.btExpenses = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHome)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIncome)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbExpenses)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogout)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxIncome)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxExpenses)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxBalance)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btIncome)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btExpenses)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbHome
+            // 
+            this.pbHome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(120)))), ((int)(((byte)(120)))));
+            this.pbHome.Image = ((System.Drawing.Image)(resources.GetObject("pbHome.Image")));
+            this.pbHome.Location = new System.Drawing.Point(76, 178);
+            this.pbHome.Name = "pbHome";
+            this.pbHome.Size = new System.Drawing.Size(160, 125);
+            this.pbHome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbHome.TabIndex = 0;
+            this.pbHome.TabStop = false;
+            this.pbHome.Click += new System.EventHandler(this.pbHome_Click);
+            // 
+            // pbIncome
+            // 
+            this.pbIncome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(120)))), ((int)(((byte)(120)))));
+            this.pbIncome.Image = ((System.Drawing.Image)(resources.GetObject("pbIncome.Image")));
+            this.pbIncome.Location = new System.Drawing.Point(72, 334);
+            this.pbIncome.Name = "pbIncome";
+            this.pbIncome.Size = new System.Drawing.Size(170, 120);
+            this.pbIncome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbIncome.TabIndex = 1;
+            this.pbIncome.TabStop = false;
+            this.pbIncome.Click += new System.EventHandler(this.pbIncome_Click);
+            // 
+            // pbExpenses
+            // 
+            this.pbExpenses.Image = ((System.Drawing.Image)(resources.GetObject("pbExpenses.Image")));
+            this.pbExpenses.Location = new System.Drawing.Point(72, 483);
+            this.pbExpenses.Name = "pbExpenses";
+            this.pbExpenses.Size = new System.Drawing.Size(170, 150);
+            this.pbExpenses.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbExpenses.TabIndex = 2;
+            this.pbExpenses.TabStop = false;
+            this.pbExpenses.Click += new System.EventHandler(this.pbExpenses_Click);
+            // 
+            // pbLogout
+            // 
+            this.pbLogout.Image = ((System.Drawing.Image)(resources.GetObject("pbLogout.Image")));
+            this.pbLogout.Location = new System.Drawing.Point(104, 669);
+            this.pbLogout.Name = "pbLogout";
+            this.pbLogout.Size = new System.Drawing.Size(104, 55);
+            this.pbLogout.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbLogout.TabIndex = 3;
+            this.pbLogout.TabStop = false;
+            this.pbLogout.Click += new System.EventHandler(this.pbLogout_Click);
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.lblUser.Font = new System.Drawing.Font("Microsoft PhagsPa", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.ForeColor = System.Drawing.Color.White;
+            this.lblUser.Location = new System.Drawing.Point(1003, 66);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(157, 44);
+            this.lblUser.TabIndex = 4;
+            this.lblUser.Text = "Hi, User!";
+            // 
+            // boxIncome
+            // 
+            this.boxIncome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.boxIncome.Image = ((System.Drawing.Image)(resources.GetObject("boxIncome.Image")));
+            this.boxIncome.Location = new System.Drawing.Point(370, 237);
+            this.boxIncome.Name = "boxIncome";
+            this.boxIncome.Size = new System.Drawing.Size(400, 250);
+            this.boxIncome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.boxIncome.TabIndex = 5;
+            this.boxIncome.TabStop = false;
+            // 
+            // boxExpenses
+            // 
+            this.boxExpenses.Image = ((System.Drawing.Image)(resources.GetObject("boxExpenses.Image")));
+            this.boxExpenses.Location = new System.Drawing.Point(803, 237);
+            this.boxExpenses.Name = "boxExpenses";
+            this.boxExpenses.Size = new System.Drawing.Size(400, 250);
+            this.boxExpenses.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.boxExpenses.TabIndex = 6;
+            this.boxExpenses.TabStop = false;
+            // 
+            // boxBalance
+            // 
+            this.boxBalance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.boxBalance.Image = ((System.Drawing.Image)(resources.GetObject("boxBalance.Image")));
+            this.boxBalance.Location = new System.Drawing.Point(636, 533);
+            this.boxBalance.Name = "boxBalance";
+            this.boxBalance.Size = new System.Drawing.Size(300, 100);
+            this.boxBalance.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.boxBalance.TabIndex = 7;
+            this.boxBalance.TabStop = false;
+            this.boxBalance.Click += new System.EventHandler(this.boxBalance_Click);
+            // 
+            // btIncome
+            // 
+            this.btIncome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(205)))), ((int)(((byte)(150)))));
+            this.btIncome.Image = ((System.Drawing.Image)(resources.GetObject("btIncome.Image")));
+            this.btIncome.Location = new System.Drawing.Point(665, 379);
+            this.btIncome.Name = "btIncome";
+            this.btIncome.Size = new System.Drawing.Size(75, 75);
+            this.btIncome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btIncome.TabIndex = 8;
+            this.btIncome.TabStop = false;
+            this.btIncome.Click += new System.EventHandler(this.btIncome_Click);
+            // 
+            // btExpenses
+            // 
+            this.btExpenses.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(99)))), ((int)(((byte)(87)))));
+            this.btExpenses.Image = ((System.Drawing.Image)(resources.GetObject("btExpenses.Image")));
+            this.btExpenses.Location = new System.Drawing.Point(1095, 379);
+            this.btExpenses.Name = "btExpenses";
+            this.btExpenses.Size = new System.Drawing.Size(75, 75);
+            this.btExpenses.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btExpenses.TabIndex = 9;
+            this.btExpenses.TabStop = false;
+            this.btExpenses.Click += new System.EventHandler(this.btExpenses_Click);
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(120)))), ((int)(((byte)(120)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1262, 753);
+            this.Controls.Add(this.btExpenses);
+            this.Controls.Add(this.btIncome);
+            this.Controls.Add(this.boxBalance);
+            this.Controls.Add(this.boxExpenses);
+            this.Controls.Add(this.boxIncome);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.pbLogout);
+            this.Controls.Add(this.pbExpenses);
+            this.Controls.Add(this.pbIncome);
+            this.Controls.Add(this.pbHome);
+            this.Name = "Dashboard";
+            this.Text = "Dashboard";
+            ((System.ComponentModel.ISupportInitialize)(this.pbHome)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIncome)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbExpenses)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogout)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxIncome)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxExpenses)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxBalance)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btIncome)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btExpenses)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbHome;
+        private System.Windows.Forms.PictureBox pbIncome;
+        private System.Windows.Forms.PictureBox pbExpenses;
+        private System.Windows.Forms.PictureBox pbLogout;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.PictureBox boxIncome;
+        private System.Windows.Forms.PictureBox boxExpenses;
+        private System.Windows.Forms.PictureBox boxBalance;
+        private System.Windows.Forms.PictureBox btIncome;
+        private System.Windows.Forms.PictureBox btExpenses;
+    }
+}
---
+++ CostTracker/CostTracker/Dashboard.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CostTracker
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void pbHome_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbIncome_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbExpenses_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbLogout_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btIncome_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btExpenses_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boxBalance_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
