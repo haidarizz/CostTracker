445711f8d5f0089982939d32f3146328aa65f414
elzanataliw <elza.natali.w@mail.ugm.ac.id>
2022-11-01 02:22:19 +0700

    add UI ViewExpenses

---
+++ CostTracker/CostTracker/ViewExpenses.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CostTracker
+{
+    public partial class ViewExpenses : Form
+    {
+        public ViewExpenses()
+        {
+            InitializeComponent();
+        }
+
+        private void pbHome_Click(object sender, EventArgs e)
+        {
+            Dashboard dashboard = new Dashboard();
+            dashboard.Show();
+            this.Hide();
+        }
+
+        private void pbIncome_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
